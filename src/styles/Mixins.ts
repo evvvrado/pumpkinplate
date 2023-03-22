ead1def262a8b2b4c7b0bc243046b4173625de4e
Everaldo Júnior <76765966+evvvrado@users.noreply.github.com>
2023-03-22 09:10:41 -0300

    fix: mixin z

--- src/styles/Mixins.ts
+++ src/styles/Mixins.ts
@@ -1,5 +1,5 @@
-import { css } from "styled-components";
-import theme from "./Theme";
+import { css } from "styled-components"
+import theme from "./Theme"
 
 export const frame = css`
 	overflow: hidden;
@@ -10,30 +10,26 @@ export const frame = css`
 		width: 100%;
 		object-fit: cover;
 	}
-`;
+`
 
 export const center = css`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-`;
+`
 
-export const flex = (
-	direction: string,
-	align: string,
-	justify: string,
-	gap?: string | "0"
-) => `
+export const flex = (direction: string, align: string, justify: string, gap?: string | "0") => `
 	display: flex;
 	align-items: ${align};
 	justify-content: ${justify};
 	flex-direction: ${direction};
 	gap: ${gap};
-`;
+`
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 export const z = (name: any) => {
-	const index = theme.z_indexes.findIndex((item) => item == name) + 1;
-	if (!index) return;
-	return index;
-};
+	const index = theme.z_indexes.findIndex((item) => item == name) + 1
+	if (!index) return
+	return theme.z_indexes.length - index
+}
