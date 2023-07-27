dadae76396b557b9e8621cd91b51eb50bb0d07fd
Everaldo Júnior <76765966+evvvrado@users.noreply.github.com>
2023-07-27 10:15:00 -0300

    chore: patterns styled div to css snippet

--- src/styles/Patterns.ts
+++ src/styles/Patterns.ts
@@ -1,8 +1,8 @@
-import styled from "styled-components";
-import theme from "./Theme";
+import { css } from "styled-components"
+import theme from "./Theme"
 
-export const Wrapper = styled.div`
+export const Wrapper = css`
 	max-width: calc(${theme.container_area} + 48px);
 	margin: 0 auto;
 	padding: 0 24px;
-`;
+`
